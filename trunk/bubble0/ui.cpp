85620d52aff5c5d9c257c40d345e01b4b55ee19d
mederic.argentina@gmail.com <mederic.argentina@gmail.com@cb0b72a0-0d06-b54d-855f-8f256b0ebc6e>
2012-01-30 09:11:31 +0000

    bubble0: 2D et 3D with multiple variable

---
+++ trunk/bubble0/ui.cpp
@@ -0,0 +1,16 @@
+void
+ui_run_clicked(void *cbdata)
+{
+    simulation_data *sim = (simulation_data *)cbdata;
+    printf("ui_run_clicked from rank %d\n",rank);
+   sim->runMode = SIM_RUNNING;
+    VisItTimeStepChanged();
+}
+
+void set_interface(void *cbdata)
+{
+   // if(rank==0)
+    VisItUI_clicked("RUN", ui_run_clicked,cbdata);
+    VisItUI_clicked("RUN", ui_run_clicked,cbdata);
+
+}
