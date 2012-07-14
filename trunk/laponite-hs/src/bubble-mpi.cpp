bb8fda3dc591caf5f3fe71f6d000f05fa535a23a
yann.bouret@gmail.com <yann.bouret@gmail.com@cb0b72a0-0d06-b54d-855f-8f256b0ebc6e>
2012-07-14 09:24:04 +0000

    bubbles: dispatch_all/collect_all is ok

--- trunk/laponite-hs/src/bubble-mpi.cpp
+++ trunk/laponite-hs/src/bubble-mpi.cpp
@@ -56,7 +56,7 @@ void Bubble:: collect(mpi &MPI)
         if( master )
         {
             MPI_Status status;
-            fprintf( stderr, "master changed=%u\n", unsigned(spots.size) );
+            //fprintf( stderr, "master changed=%u\n", unsigned(spots.size) );
             for( int r = 1; r < MPI.CommWorldSize; ++r )
             {
                 //--------------------------------------------------------------
@@ -64,7 +64,7 @@ void Bubble:: collect(mpi &MPI)
                 //--------------------------------------------------------------
                 
                 const size_t num_changed = MPI.RecvAs<size_t>(r, tag, MPI_COMM_WORLD, status);
-                fprintf( stderr, "rank %d changed=%u\n", r, unsigned(num_changed) );
+                //fprintf( stderr, "rank %d changed=%u\n", r, unsigned(num_changed) );
                 
                 //--------------------------------------------------------------
                 // far all changed
--- trunk/laponite-hs/src/bubbles-mpi.cpp
+++ trunk/laponite-hs/src/bubbles-mpi.cpp
@@ -32,3 +32,27 @@ void Bubbles:: dispatch_all(mpi &MPI)
     
     
 }
+
+void Bubbles:: collect_all( mpi &MPI )
+{
+#if !defined (NDEBUG)
+    const bool master   = MPI.IsMaster;
+    //==========================================================================
+    // broadcast #bubbles
+    //==========================================================================
+    size_t num_bubbles = 0;
+    if( master )
+    {
+        num_bubbles = count();
+    }
+    MPI.BcastAs<size_t>(num_bubbles, 0, MPI_COMM_WORLD);
+    if( num_bubbles != count() )
+        throw exception("Invalid bubbles count to collect rank %d", MPI.CommWorldRank);
+#endif
+    
+    for( Bubble *b = b_list.head;b;b=b->next)
+    {
+        b->collect(MPI);
+    }
+    
+}
