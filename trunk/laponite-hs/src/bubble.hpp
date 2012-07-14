0feb03d2c94229eeef8e2cb8fe94f19af25e98b7
yann.bouret@gmail.com <yann.bouret@gmail.com@cb0b72a0-0d06-b54d-855f-8f256b0ebc6e>
2012-07-14 08:43:14 +0000

    added bubbles MPI

--- trunk/laponite-hs/src/bubble.hpp
+++ trunk/laponite-hs/src/bubble.hpp
@@ -3,7 +3,6 @@
 
 #include "spot.hpp"
 
-
 #if defined(HAS_MPI)
 #include "yocto/mpi/mpi.hpp"
 #endif
@@ -11,17 +10,17 @@
 // TODO: update/update area/update differential ppties
 
 //! a non intersecting polygon
-class Bubble : public Point::List
+class Bubble :  public Point::List
 {
 public:
     explicit Bubble( Point::Pool &pcache, Spot::Pool &scache ) throw();
     virtual ~Bubble() throw();
     
-    double     lambda; //!< critical length, default is 1
-    double     area;   //!< area, to be broadcasted
-    Spot::List spots;  //!< keep trace of points
-    bool       active; //!< spots.size > 0 
-    
+    double         lambda; //!< critical length, default is 1
+    double         area;   //!< area, to be broadcasted
+    Spot::List     spots;  //!< keep trace of points
+    bool           active; //!< spots.size > 0 
+      
     void   update_points(); // update #points
     void   update_area() throw(); //!< area = evaluate_area
     double evaluate_area() const throw(); //!< evaluate area, doesn't set it !
--- trunk/laponite-hs/src/bubbles.hpp
+++ trunk/laponite-hs/src/bubbles.hpp
@@ -4,18 +4,31 @@
 
 #include "bubble.hpp"
 
-typedef core::list_of<Bubble> BubbleList;
 
-class Bubbles : public BubbleList
+class Bubbles 
 {
 public:
     explicit Bubbles() throw();
     virtual ~Bubbles() throw();
     
+    size_t         count() const throw();
+    Bubble *       first() throw();
+    const Bubble * first() const throw();
+    
+    void   none() throw();
+    
+    Bubble &create();
+    
+#if defined(HAS_MPI)
+    void dispatch_all( mpi &MPI );
+#endif
     
 private:
-    Point::Pool pcache;
-    Spot::Pool  scache;
+    core::list_of<Bubble> b_list;
+    core::pool_of<Bubble> b_pool;
+    Point::Pool           pcache;
+    Spot::Pool            scache;
+    
     YOCTO_DISABLE_COPY_AND_ASSIGN(Bubbles);
 };
 
