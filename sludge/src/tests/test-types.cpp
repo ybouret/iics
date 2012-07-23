c180994ba373602faa395d7a08791dc334e07275
yann.bouret@gmail.com <yann.bouret@gmail.com@cb0b72a0-0d06-b54d-855f-8f256b0ebc6e>
2012-07-23 06:52:37 +0000

    better laponite

---
+++ sludge/src/tests/test-types.cpp
@@ -0,0 +1,8 @@
+#include "yocto/utest/run.hpp"
+#include "../types.hpp"
+
+YOCTO_UNIT_TEST_IMPL(types)
+{
+
+}
+YOCTO_UNIT_TEST_DONE()
---
+++ sludge/src/types.hpp
@@ -0,0 +1,7 @@
+#ifndef TYPES_INCLUDED
+#define TYPES_INCLUDED 1
+
+#include "yocto/swamp/array2d.hpp"
+
+
+#endif
