e8c14f6d034abcebbcb15a6f9de1124b50a26712
yann.bouret@gmail.com <yann.bouret@gmail.com@cb0b72a0-0d06-b54d-855f-8f256b0ebc6e>
2012-07-24 10:06:32 +0000

    check PBC

--- sludge/src/tests/test-seg.cpp
+++ sludge/src/tests/test-seg.cpp
@@ -23,81 +23,77 @@ YOCTO_UNIT_TEST_IMPL(seg)
     vector<string> var;
     var.push_back( "B" );
     
-    { 
-        const Layout  lay( Coord(0,0), Coord(20,30) );
-        const Region  reg( Vertex(0,pbc.lo/2), Vertex(box.x,pbc.up/2) );
-        workspace<Layout, Real, rmesh> W(lay, gs, fs);
-        W.mesh.regular_map_to(reg, lay);
-        SaveGrid(W.mesh,"grid0.dat");
-        Segmenter Seg( W.mesh );
-        Array &B = W["B"].as<Array>();
-        vtk_writer vtk;
-        
-        {
+    const Layout  lay( Coord(0,0), Coord(20,30) );
+    const Region  reg( Vertex(0,pbc.lo/2), Vertex(box.x,pbc.up/2) );
+    workspace<Layout, Real, rmesh> W(lay, gs, fs);
+    W.mesh.regular_map_to(reg, lay);
+    SaveGrid(W.mesh,"grid0.dat");
+    Segmenter Seg( W.mesh );
+    Array &B = W["B"].as<Array>();
+    vtk_writer vtk;
     
-            bubbles.lambda = min_of( Seg.dX[0], Seg.dY[0])/2;
-        }
-        
-        Seg.allocate_segments();
-        
-        //-- small bubble
-        bubbles.create()->map_circle(center, 2);
-        bubbles.check_topologies();
-        bubbles.first()->save_dat("bubble0.dat");
-        
-        bubbles.check_geometries_within( reg.vmin.y, reg.vmax.y);
-
-        
-        Seg.process_bubbles( bubbles );
-        Seg.save_junctions("junc0.dat");
-        bubbles.fill( B );
-        vtk.save("b0.vtk", "b0", W, var, W.__layout());
-        
-        //-- bigger bubble
-        bubbles.empty();
-        
-        bubbles.create()->map_circle(center, 3);
-        bubbles.check_topologies();
-        bubbles.first()->save_dat("bubble1.dat");
-        
-        bubbles.check_geometries_within( reg.vmin.y, reg.vmax.y);
-        
-        
-        Seg.process_bubbles( bubbles );
-        Seg.save_junctions("junc1.dat");
-        bubbles.fill( B );
-        vtk.save("b1.vtk", "b1", W, var, W.__layout());
-
-        //-- peanut
-        bubbles.empty();
-        
-        bubbles.create()->map_peanut(center, 3.5, 0.95);
-        bubbles.check_topologies();
-        bubbles.first()->save_dat("bubble2.dat");
-        
-        bubbles.check_geometries_within( reg.vmin.y, reg.vmax.y);
-        
-        
-        Seg.process_bubbles( bubbles );
-        Seg.save_junctions("junc2.dat");
-        bubbles.fill( B );
-        vtk.save("b2.vtk", "b2", W, var, W.__layout());
-        
-        //-- peanut with shift
-        bubbles.empty();
-        
-        bubbles.create()->map_peanut(center+Vertex(0,1), 3.5, 0.95);
-        bubbles.check_topologies();
-        bubbles.first()->save_dat("bubble3.dat");
-        
-        bubbles.check_geometries_within( reg.vmin.y, reg.vmax.y);
-        
-        
-        Seg.process_bubbles( bubbles );
-        Seg.save_junctions("junc3.dat");
-        bubbles.fill( B );
-        vtk.save("b3.vtk", "b3", W, var, W.__layout());
-    }
+    
+    bubbles.lambda = min_of( Seg.dX[0], Seg.dY[0])/2;
+    
+    Seg.allocate_segments();
+    
+    //-- small bubble
+    bubbles.create()->map_circle(center, 2);
+    bubbles.check_topologies();
+    bubbles.first()->save_dat("bubble0.dat");
+    
+    bubbles.check_geometries_within( reg.vmin.y, reg.vmax.y);
+    
+    
+    Seg.process_bubbles( bubbles );
+    Seg.save_junctions("junc0.dat");
+    bubbles.fill( B );
+    vtk.save("b0.vtk", "b0", W, var, W.__layout());
+    
+    //-- bigger bubble
+    bubbles.empty();
+    
+    bubbles.create()->map_circle(center, 3);
+    bubbles.check_topologies();
+    bubbles.first()->save_dat("bubble1.dat");
+    
+    bubbles.check_geometries_within( reg.vmin.y, reg.vmax.y);
+    
+    
+    Seg.process_bubbles( bubbles );
+    Seg.save_junctions("junc1.dat");
+    bubbles.fill( B );
+    vtk.save("b1.vtk", "b1", W, var, W.__layout());
+    
+    //-- peanut
+    bubbles.empty();
+    
+    bubbles.create()->map_peanut(center, 3.5, 0.95);
+    bubbles.check_topologies();
+    bubbles.first()->save_dat("bubble2.dat");
+    
+    bubbles.check_geometries_within( reg.vmin.y, reg.vmax.y);
+    
+    
+    Seg.process_bubbles( bubbles );
+    Seg.save_junctions("junc2.dat");
+    bubbles.fill( B );
+    vtk.save("b2.vtk", "b2", W, var, W.__layout());
+    
+    //-- peanut with shift
+    bubbles.empty();
+    
+    bubbles.create()->map_peanut(center+Vertex(0,1), 3.5, 0.95);
+    bubbles.check_topologies();
+    bubbles.first()->save_dat("bubble3.dat");
+    
+    bubbles.check_geometries_within( reg.vmin.y, reg.vmax.y);
+    
+    
+    Seg.process_bubbles( bubbles );
+    Seg.save_junctions("junc3.dat");
+    bubbles.fill( B );
+    vtk.save("b3.vtk", "b3", W, var, W.__layout());
     
 }
 YOCTO_UNIT_TEST_DONE()
