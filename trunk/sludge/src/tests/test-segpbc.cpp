32f69a149b79b89dc22aa7540f224019528a4181
yann.bouret@gmail.com <yann.bouret@gmail.com@cb0b72a0-0d06-b54d-855f-8f256b0ebc6e>
2012-07-24 10:06:32 +0000

    check PBC

---
+++ trunk/sludge/src/tests/test-segpbc.cpp
@@ -0,0 +1,59 @@
+#include "yocto/utest/run.hpp"
+#include "../segmenter.hpp"
+#include "yocto/code/utils.hpp"
+#include "yocto/swamp/vtk-writer.hpp"
+
+YOCTO_UNIT_TEST_IMPL(segpbc)
+{
+
+    //==========================================================================
+    //
+    // prepare the grid
+    // 
+    //==========================================================================
+    const Vertex  box(10,10);
+    const Vertex  center( box.x/2, 0);
+    Bubbles       bubbles( box );
+    const PBC    &pbc = bubbles.pbc;
+    
+    GhostsSetup   gs;
+    FieldsSetup   fs;
+    
+    Y_SWAMP_DECL_AUX(fs, "B", Array);
+    vector<string> var;
+    var.push_back( "B" );
+    
+    const Layout  lay( Coord(0,0), Coord(20,30) );
+    const Region  reg( Vertex(0,pbc.lo), Vertex(box.x,pbc.up) );
+    workspace<Layout, Real, rmesh> W(lay, gs, fs);
+    W.mesh.regular_map_to(reg, lay);
+    SaveGrid(W.mesh,"grid0.dat");
+    Segmenter Seg( W.mesh );
+    Array &B = W["B"].as<Array>();
+    vtk_writer vtk;
+    
+    
+    bubbles.lambda = min_of( Seg.dX[0], Seg.dY[0])/2;
+    
+    Seg.allocate_segments();
+    
+    //-- make a peanut
+    bubbles.empty();
+    
+    bubbles.create()->map_peanut(center+Vertex(0,-4), 3.5, 0.95);
+    bubbles.check_topologies();
+    bubbles.first()->save_dat("bubble0.dat");
+    
+    
+    bubbles.check_geometries_within( reg.vmin.y, reg.vmax.y);
+    
+    
+    Seg.process_bubbles( bubbles );
+    Seg.save_junctions("junc0.dat");
+    bubbles.fill( B );
+    vtk.save("b0.vtk", "b0", W, var, W.__layout());
+
+    
+    
+}
+YOCTO_UNIT_TEST_DONE()
