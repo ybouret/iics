d8c30aa63e9d36d587f6d076bcd50f956631f6b3
yann.bouret@gmail.com <yann.bouret@gmail.com@cb0b72a0-0d06-b54d-855f-8f256b0ebc6e>
2012-07-26 09:02:49 +0000

    ready for visit...

--- trunk/sludge/src/cell-dispatch.cpp
+++ trunk/sludge/src/cell-dispatch.cpp
@@ -12,14 +12,14 @@ void Cell:: check_borders()
             //------------------------------------------------------------------
             // send/recv how many
             //------------------------------------------------------------------
-            fprintf( stderr, "%d should send %lu segments to %d\n", sim_rank, border_segments->size, border_peer );
+            //fprintf( stderr, "%d should send %lu segments to %d\n", sim_rank, border_segments->size, border_peer );
             const size_t self_ns = border_segments->size;
             size_t       peer_ns = 0;
             MPI_Status   status;
             MPI.Sendrecv(&self_ns, sizeof(self_ns), MPI_BYTE, border_peer, segtag,
                          &peer_ns, sizeof(peer_ns), MPI_BYTE, border_peer, segtag, 
                          MPI_COMM_WORLD,status );
-            fprintf( stderr, "%d will recv %lu segments from %d\n",sim_rank,peer_ns,border_peer);
+            //fprintf( stderr, "%d will recv %lu segments from %d\n",sim_rank,peer_ns,border_peer);
             
             //------------------------------------------------------------------
             // prepare resources
@@ -44,7 +44,7 @@ void Cell:: check_borders()
             // and check/merge...
             //------------------------------------------------------------------
             //MPI.__WaitFor(0.5*sim_rank);
-            fprintf( stderr, "\ncheck_borders on rank %d\n", sim_rank );
+            //fprintf( stderr, "\ncheck_borders on rank %d\n", sim_rank );
             segmenter.merge_pbc(*border_segments, border_y, peer_border, border_y);
             peer_border.empty();
         }
