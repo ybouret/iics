40d196c925fb7faa21beda0d408c96a91f94e25c
yann.bouret@gmail.com <yann.bouret@gmail.com@cb0b72a0-0d06-b54d-855f-8f256b0ebc6e>
2012-07-31 08:02:03 +0000

    replaced by trigonometric interpolation

--- trunk/sludge/src/tests/test-fourier.cpp
+++ trunk/sludge/src/tests/test-fourier.cpp
@@ -1,7 +1,6 @@
 #include "yocto/utest/run.hpp"
 #include "../bubble.hpp"
 #include "yocto/ios/ocstream.hpp"
-#include "yocto/math/kernel/linsys.hpp"
 #include "yocto/code/utils.hpp"
 
 static inline
@@ -19,113 +18,55 @@ void expand( Bubble &bubble , const Vertex &center)
 
 
 
-static inline
-void scale_fourier( Bubble &bubble )
+#include "yocto/math/dat/trigonometric.hpp"
+
+void scale_trigo( Bubble &bubble )
 {
-    vector<Real> s;
-    vector<Real> a;
-    vector<Real> b;
+    vector<Real>    s;
+    vector<Real>    ax;
+    vector<Real>    ay;
     
+    const size_t n      = bubble.size;
+    Tracer      *p      = bubble.root;
+    Real         period = 0;
+    std::cerr << "trigo for " << n << " points" << std::endl;
     
-    const size_t    n = bubble.size;
-    const bool      is_even = 0 == (n&1);
-    const size_t    nn = (n >> 1) - ( is_even ? 1 : 0 );
-    std::cerr << "n=" << n << ", nn=" << nn << std::endl;
-    Tracer *p      = bubble.root;
-    Real    period = 0;
     for( size_t i=n;i>0;--i,p=p->next)
     {
         bubble.pbc(p->vertex);
         {
             s.push_back(period);
             
-            a.push_back(p->vertex.x);
-            b.push_back(p->vertex.y);
+            ax.push_back(p->vertex.x);
+            ay.push_back(p->vertex.y);
             
         }
         Vertex edge(p->vertex,p->next->vertex);
         bubble.pbc(edge);
         period += edge.norm();
     }
+
+    const Real   tfac = numeric<Real>::two_pi / period;
+    vector<Real> theta(s);
+    for( size_t i=n;i>0;--i) theta[i] *= tfac;
+    linsys<Real>        solver;
+    trigonometric<Real> trig( theta, solver );
+    trig.compute(ax, solver);
+    trig.compute(ay, solver);
     
-    const double tfac = numeric<Real>::two_pi/period;
     
-    
-    matrix<Real> M(n,n);
-    for( size_t i=1; i <=n; ++i )
-    {
-        const Real t_i = s[i] * tfac;
-        size_t j       = 1;
-        M[i][j++] = 1;
-        for( size_t k=1; k <=nn; ++k )
-        {
-            const Real    arg = k * t_i;
-            M[i][j++] = Cos(arg);
-            M[i][j++] = Sin(arg);
-        }
-        if(is_even)
-            M[i][j] = Cos( (nn+1)*t_i );
-    }
-    
-    {
-        ios::ocstream fp("source.dat", false);
-        for( size_t i=1; i <= n; ++i )
-        {
-            fp("%g %g %g\n", s[i]*tfac, a[i], b[i] );
-        }
-    }
-    
-    linsys<Real> solver(n);
-    if( !solver.LU(M) )
-    {
-        throw exception("Matrix is not invertible");
-    }
-    solver(M,a);
-    solver(M,b);
-    //std::cerr << "a=" << a << std::endl;
-    //std::cerr << "b=" << b << std::endl;
-    
-    const size_t m = max_of<size_t>(3,ceil( period/ bubble.lambda ));
+    const size_t m = 5*max_of<size_t>(3,ceil( period/ bubble.lambda ));
     std::cerr << "Placing " << m << " points from " << n << std::endl;
     const double ds = period/m;
     bubble.empty();
-    ios::ocstream fp("target.dat",false);
-    const Real t0 = Alea() * numeric<Real>::two_pi;
+
     for( size_t i=0; i < m; ++i )
     {
-        const Real t_i = t0 + (i*ds) * tfac;
-        size_t     j=1;
-        
-        Real x = a[j];
-        Real y = b[j];
-        ++j;
-        for( size_t k=1; k <= nn; ++k )
-        {
-            const Real    arg = k * t_i;
-            const Real    ca  = Cos(arg);
-            const Real    sa  = Sin(arg);
-            x += a[j]   * ca;
-            y += b[j]   * ca;
-            ++j;
-            x += a[j]   * sa;
-            y += b[j]   * sa;
-            ++j;
-        }
-         if(is_even)
-         {
-             const Real arg = (nn+1) * t_i;
-             x += a[j] * Cos( arg );
-             y += b[j] * Sin( arg );
-         }
-        fp("%g %g %g\n", t_i, x, y );
-        Vertex &v = bubble.append()->vertex;
-        v.x = x;
-        v.y = y;
-    
+        const Real t_i = (i*ds) * tfac;
+        bubble.append()->vertex = trig( t_i, ax, ay);
     }
+    
 }
-
-
 
 YOCTO_UNIT_TEST_IMPL(fourier)
 {
@@ -141,24 +82,22 @@ YOCTO_UNIT_TEST_IMPL(fourier)
     
     Bubble bubble(lambda,pbc,tcache,scache,mcache);
     
-    bubble.map_circle( center, 2.0);
+    bubble.map_circle( center, 2.0 + Alea() * 0.2 );
     bubble.save_dat("circle0.dat");
-    scale_fourier(bubble);
+    scale_trigo(bubble);
     bubble.save_dat("circle1.dat");
     expand(bubble,center);
     bubble.save_dat("circle2.dat");
-    scale_fourier(bubble);
+    scale_trigo(bubble);
     bubble.save_dat("circle3.dat");
     
-    bubble.map_peanut( center, 2, 0.96);
+    bubble.map_peanut( center, 2+Alea()*0.2, 0.96);
     bubble.save_dat("peanut0.dat");
-    scale_fourier(bubble);
+    scale_trigo(bubble);
     bubble.save_dat("peanut1.dat");
-
     expand(bubble,center);
     bubble.save_dat("peanut2.dat");
-    scale_fourier(bubble);
+    scale_trigo(bubble);
     bubble.save_dat("peanut3.dat");
-    
 }
 YOCTO_UNIT_TEST_DONE()
