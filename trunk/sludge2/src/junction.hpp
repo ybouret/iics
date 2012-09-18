5d24b45e0decb50277686f543cdcee32321944af
yann.bouret@gmail.com <yann.bouret@gmail.com@cb0b72a0-0d06-b54d-855f-8f256b0ebc6e>
2012-09-18 08:00:25 +0000

    start segmenter

---
+++ trunk/sludge2/src/junction.hpp
@@ -0,0 +1,28 @@
+#ifndef JUNCTION_INCLUDED
+#define JUNCTION_INCLUDED 1
+
+#include "types.hpp"
+#include "yocto/sequence/cached-list.hpp"
+#include "yocto/core/list.hpp"
+
+//! bubble/mesh junctions
+class Junction
+{
+public:
+    Junction( const Vertex &at) throw();
+    ~Junction() throw();
+    
+    Junction       *next;
+    Junction       *prev;
+    const Vertex    pos;
+    
+    typedef cache_of<Junction> Cache;
+    
+private:
+    YOCTO_DISABLE_COPY_AND_ASSIGN(Junction);
+};
+
+typedef cached_list<core::list_of, Junction> Junctions;
+
+#endif
+
