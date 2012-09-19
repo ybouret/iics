071a421af156b207b10b72a64741fe32fb15caf0
yann.bouret@gmail.com <yann.bouret@gmail.com@cb0b72a0-0d06-b54d-855f-8f256b0ebc6e>
2012-09-19 15:22:29 +0000

    tested parameters: need MPI(+visit)

---
+++ sludge2/src/tests-mpi/test-param.cpp
@@ -0,0 +1,27 @@
+#include "yocto/utest/run.hpp"
+#include "../parameters.hpp"
+
+
+YOCTO_UNIT_TEST_IMPL(param)
+{
+    const mpi &MPI = mpi::init(&argc,&argv);
+    const Coord  N(10,20);
+    const Vertex L(2.0,3.0);
+    Parameters   param(N,L,MPI.CommWorldRank,MPI.CommWorldSize);
+    array_db     adb;
+    Grid         grid(param.sim_layout,adb);
+    
+    param.setup_grid(grid);
+    
+    SaveGrid(grid, vformat("g%d.%d.dat", MPI.CommWorldSize, MPI.CommWorldRank) );
+    
+    MPI.__WaitFor(MPI.CommWorldRank*0.5);
+    std::cerr << MPI.CommWorldSize << "." << MPI.CommWorldRank << std::endl;
+    std::cerr << "full_layout: " << param.full_layout << std::endl;
+    std::cerr << "sim_layout : " << param.sim_layout << std::endl;
+
+    std::cerr.flush();
+    MPI.Barrier(MPI_COMM_WORLD);
+}
+YOCTO_UNIT_TEST_DONE()
+
