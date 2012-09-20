ca4e42bde4d5630b028eea094cfb6d8c7330866b
yann.bouret@gmail.com <yann.bouret@gmail.com@cb0b72a0-0d06-b54d-855f-8f256b0ebc6e>
2012-09-20 09:42:26 +0000

    dispatching cell's bubble

--- sludge2/src/mpi/cell.cpp
+++ sludge2/src/mpi/cell.cpp
@@ -11,14 +11,35 @@ Cell:: Cell(const mpi         &MPI,
             const FieldsSetup &F ) :
 Parameters(N,L,MPI.CommWorldRank,MPI.CommWorldSize),
 Workspace( sim_layout, F, sim_ghosts ),
+X( mesh.X() ),
+Y( mesh.Y() ),
 segmenter( mesh ),
-bubbles( pbc )
+bubbles( pbc ),
+ymin(0),
+ymax(0)
 {
+    
     //-- compute the grid
     setup_grid( mesh );
     
-    //-- create segmenter
+    //-- tune the data
+    (Real&)ymin = Y[ this->lower.y ];
+    (Real&)ymax = Y[ this->upper.y ];
+    MPI.PrintfI(stderr, "ymin=%g / ymax=%g / pbc=[%g;%g]\n", ymin, ymax, pbc.lo, pbc.up);
+    
+    //-- create segments
     segmenter.create();
+    
     
 }
 
+void Cell:: dispatch( const mpi &MPI )
+{
+    MPI.Printf0(stderr, "\tdispatch bubbles...\n");
+    bubbles.dispatch(MPI);
+    MPI.Printf0(stderr, "\tlocate spots...\n");
+    for( Bubble *b = bubbles.first(); b;b=b->next)
+    {
+        b->locate_spots(ymin, ymax);
+    }
+}
