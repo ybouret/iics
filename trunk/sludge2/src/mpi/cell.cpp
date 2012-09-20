e5d049dc15381d1c6086efbf817789d0d08d01e2
yann.bouret@gmail.com <yann.bouret@gmail.com@cb0b72a0-0d06-b54d-855f-8f256b0ebc6e>
2012-09-20 07:26:35 +0000

    basic cell, need to test

--- trunk/sludge2/src/mpi/cell.cpp
+++ trunk/sludge2/src/mpi/cell.cpp
@@ -1,1 +1,24 @@
 #include "cell.hpp"
+
+Cell:: ~Cell() throw()
+{
+    
+}
+
+Cell:: Cell(const mpi         &MPI,
+            const Coord       &N,
+            const Vertex      &L,
+            const FieldsSetup &F ) :
+Parameters(N,L,MPI.CommWorldRank,MPI.CommWorldSize),
+Workspace( sim_layout, F, sim_ghosts ),
+segmenter( mesh ),
+bubbles( pbc )
+{
+    //-- compute the grid
+    setup_grid( mesh );
+    
+    //-- create segmenter
+    segmenter.create();
+    
+}
+
--- trunk/sludge2/src/mpi/cell.hpp
+++ trunk/sludge2/src/mpi/cell.hpp
@@ -1,11 +1,28 @@
 #ifndef CELL_INCLUDED
 #define CELL_INCLUDED
 
-#include "types.hpp"
+#include "../parameters.hpp"
 #include "yocto/spade/mpi/workspace.hpp"
 
+typedef mpi_workspace<Layout,rmesh,Real> Workspace;
 
-
+//! Hele Shaw Cell
+class Cell : public Parameters, public Workspace
+{
+public:
+    explicit Cell(const mpi         &MPI,
+                  const Coord       &N,
+                  const Vertex      &L,
+                  const FieldsSetup &F);
+    
+    virtual ~Cell() throw();
+    
+    Segmenter segmenter;
+    Bubbles   bubbles;
+    
+private:
+    YOCTO_DISABLE_COPY_AND_ASSIGN(Cell);
+};
 
 #endif
 
