c99d2449f431ec4e9f68591556b86ebb7c55b6af
yann.bouret@gmail.com <yann.bouret@gmail.com@cb0b72a0-0d06-b54d-855f-8f256b0ebc6e>
2012-09-25 07:28:39 +0000

    pressurize seems ok

--- trunk/sludge2/src/mpi/cell/pressure.cpp
+++ trunk/sludge2/src/mpi/cell/pressure.cpp
@@ -2,10 +2,14 @@
 
 void Cell:: init_pressure(const mpi &MPI )
 {
-    //! bubble
+    //! pressure from bubbles
     segmenter.pressurize(P);
     
     //! boundary conditions
+    for( unit_t j=lower.y;j<=upper.y;++j)
+    {
+        P[j][upper.x] = 0.5;
+    }
     
     //! sync pressure
     sync1(MPI,P);
