c5da034dca24368d3b128d8ad418be0c17ebedeb
yann.bouret@gmail.com <yann.bouret@gmail.com@cb0b72a0-0d06-b54d-855f-8f256b0ebc6e>
2012-09-27 14:13:39 +0000

    need a check from yocto4 ghosts...

--- sludge2/src/mpi/cell/dispatch.cpp
+++ sludge2/src/mpi/cell/dispatch.cpp
@@ -18,17 +18,24 @@ void Cell:: dispatch( const mpi &MPI )
     MPI.Printf0(stderr, "\tbuild bubble field...\n");
     segmenter.build(B);
     
-    MPI.Printf0(stderr, "\tsync bubble field...\n");
-    sync1(MPI,B);
-    
-#if 1
+#define SAVE_B
+#if defined(SAVE_B)
     variables bvar;
     bvar.append( "B" );
-    vtk.save( vformat("b-out-%d.%d.vtk", MPI.CommWorldSize,MPI.CommWorldRank),
+    vtk.save( vformat("b-core-%d.%d.vtk", MPI.CommWorldSize,MPI.CommWorldRank),
              "B full layout", *this,
              bvar,
              outline);
+#endif
     
+    MPI.Printf0(stderr, "\tsync bubble field...\n");
+    sync1(MPI,B);
+    
+#if defined(SAVE_B)
+    vtk.save( vformat("b-sync-%d.%d.vtk", MPI.CommWorldSize,MPI.CommWorldRank),
+             "B full layout", *this,
+             bvar,
+             outline);
     segmenter.save_vtk_n( vformat("j%d-%d.vtk",MPI.CommWorldSize,MPI.CommWorldRank), bubbles.lambda);
 #endif
     
