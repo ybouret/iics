ef58fb4ef5333b7d6f74fedaaa447403fc87334a
yann.bouret@gmail.com <yann.bouret@gmail.com@cb0b72a0-0d06-b54d-855f-8f256b0ebc6e>
2012-10-04 12:28:10 +0000

    Ok for JPack I/O

--- sludge2/src/mpi/segmenter.cpp
+++ sludge2/src/mpi/segmenter.cpp
@@ -3,21 +3,23 @@
 void Segmenter:: dispatch_vertical_junctions( const mpi &MPI, Cell &cell )
 {
     static const int tag = 0xD15;
-    const int source = MPI.CommWorldLast;
-    const int target = 0;
-    const int rank   = MPI.CommWorldRank;
+    const int  source = MPI.CommWorldLast;
+    const int  target = 0;
+    const int  rank   = MPI.CommWorldRank;
     const bool is_source = rank == source;
     const bool is_target = rank == target;
+    //const bool is_active = is_source || is_target;
     
     
-    MPI_Request      request;
+    MPI_Request      source_request;
+    MPI_Request      target_request;
     unsigned long    count = 0;
+    vector<JPack>   &jcom  = cell.jcom;
     if( is_source )
     {
         //----------------------------------------------------------------------
         // encoding extraneous vertical junctions
         //----------------------------------------------------------------------
-        vector<JPack> &jcom = cell.jcom;
         jcom.free();
         assert(0==jcom.size());
         for( unit_t i=X.lower;i<=X.upper;++i)
@@ -31,31 +33,76 @@ void Segmenter:: dispatch_vertical_junctions( const mpi &MPI, Cell &cell )
                 J = J->prev;
             }
         }
+        
+        //----------------------------------------------------------------------
+        // sending #count to target
+        //----------------------------------------------------------------------
         count = jcom.size();
         fprintf( stderr, "\t@source: Need to send %lu >= %g\n",count,cell.Y[cell.upper.y] );
         for(size_t i=1;i<=count;++i)
         {
-            fprintf( stderr , "\t\t@%ld: bubble #%u: y=%g\n", jcom[i].i, jcom[i].b, jcom[i].y);
+            fprintf( stderr , "\t\t-->@%ld: bubble #%u: y=%g\n", jcom[i].i, jcom[i].b, jcom[i].y);
         }
-        MPI.Isend(&count, 1, MPI_UNSIGNED_LONG, target, tag, MPI_COMM_WORLD, request);
+        MPI.Isend(&count, 1, MPI_UNSIGNED_LONG, target, tag, MPI_COMM_WORLD, source_request);
     }
+    
     
     if( is_target )
     {
-        MPI.Irecv(&count, 1, MPI_UNSIGNED_LONG, source, tag, MPI_COMM_WORLD, request);
+        //----------------------------------------------------------------------
+        // recv #count
+        //----------------------------------------------------------------------
+        MPI.Irecv(&count, 1, MPI_UNSIGNED_LONG, source, tag, MPI_COMM_WORLD, target_request);
     }
     
-    if( is_source || is_target )
+    if( is_source )
     {
+        //----------------------------------------------------------------------
+        // wait for send of #count
+        //----------------------------------------------------------------------
         MPI_Status status;
-        MPI.Wait(request, status);
+        MPI.Wait(source_request, status);
+        
+        //----------------------------------------------------------------------
+        // send data to target
+        //----------------------------------------------------------------------
+        MPI.Isend( jcom(), count * sizeof(JPack), MPI_BYTE, target, tag, MPI_COMM_WORLD, source_request);
     }
     
     if( is_target)
     {
+        //----------------------------------------------------------------------
+        // wait for recv of #count
+        //----------------------------------------------------------------------
+        MPI_Status status;
+        MPI.Wait(target_request, status);
+        
+        //----------------------------------------------------------------------
+        // ok, now recv data from source
+        //----------------------------------------------------------------------
         fprintf( stderr, "\t@target: Need to recv %lu\n",count );
+        const JPack invalid_jpack;
+        jcom.make( count, invalid_jpack);
+        MPI.Irecv( jcom(), count * sizeof(JPack), MPI_BYTE, source, tag, MPI_COMM_WORLD, target_request);
     }
     
+    if( is_source )
+    {
+        MPI_Status status;
+        MPI.Wait(source_request, status);
+        // source is done
+    }
+    
+    if( is_target )
+    {
+        MPI_Status status;
+        MPI.Wait(target_request, status);
+        for( size_t k=1;k<=count;++k)
+        {
+            const JPack &jpack = jcom[k];
+            fprintf( stderr , "\t\t<--@%ld: bubble #%u: y=%g\n", jpack.i, jpack.b, jpack.y);
+        }
+    }
     
 }
 
