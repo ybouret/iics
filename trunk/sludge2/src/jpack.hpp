a58d7ab38c94e5505c5f18e66a53c9c22a7ae9f4
yann.bouret@gmail.com <yann.bouret@gmail.com@cb0b72a0-0d06-b54d-855f-8f256b0ebc6e>
2012-10-04 09:28:38 +0000

    start encoding vertical junctions

---
+++ trunk/sludge2/src/jpack.hpp
@@ -0,0 +1,21 @@
+#ifndef JPACK_INCLUDED
+#define JPACK_INCLUDED 1
+
+#include "junction.hpp"
+
+//! Vertical Junction Packing
+class JPack
+{
+public:
+    JPack( unit_t idx, const Junction *J ) throw();
+    ~JPack() throw();
+    JPack( const JPack &) throw();
+    const unit_t   i; //!< abscissa index
+    const BubbleID b; //!< which bubble
+    const Real     y; //!< junction Y
+    
+private:
+    YOCTO_DISABLE_ASSIGN(JPack);
+};
+
+#endif
