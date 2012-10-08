17bfab8551fec816eb1b17c4694f5d0566994961
yann.bouret@gmail.com <yann.bouret@gmail.com@cb0b72a0-0d06-b54d-855f-8f256b0ebc6e>
2012-10-08 07:55:07 +0000

    a more complex test case

--- trunk/sludge2/src/laponite/main.cpp
+++ trunk/sludge2/src/laponite/main.cpp
@@ -32,8 +32,8 @@ int main( int argc, char *argv[] )
         //----------------------------------------------------------------------
         // Setup Simulation
         //----------------------------------------------------------------------
-        const Coord  N(30,60);
-        const Vertex L(3.0,5.0);
+        const Coord  N(70,60);
+        const Vertex L(6.0,5.0);
         Simulation   sim(MPI,N,L);
         MPI.PrintfI(stderr, "layout: (%d,%d) -> (%d,%d)\n", int(sim.lower.x),int(sim.lower.y),int(sim.upper.x),int(sim.upper.y));
         MPI.WaitFor(0.1);
