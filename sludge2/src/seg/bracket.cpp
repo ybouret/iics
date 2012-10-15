2abe97c09b93a7b3058d30ef77aa8927e3b6c995
yann.bouret@gmail.com <yann.bouret@gmail.com@cb0b72a0-0d06-b54d-855f-8f256b0ebc6e>
2012-10-15 15:46:35 +0000

    ok, bracketing tracers with junctions

--- sludge2/src/seg/bracket.cpp
+++ sludge2/src/seg/bracket.cpp
@@ -6,20 +6,79 @@ void Segmenter:: find_bracketing_junctions(const Spot *spot) const
 {
     assert(spot);
     
-    //--------------------------------------------------------------------------
-    // Use spot info on the grid
-    //--------------------------------------------------------------------------
-    const Coord   klo    = spot->klo;
-    const Coord   kup    = spot->kup;
     const Tracer *tracer = spot->handle; assert(tracer);
-    const Vertex  v      = tracer->vertex;
-   
-#if 0
-    const Junctions &HSegLo = Horz(klo.y);
-    const Junctions &HSegUp = Horz(kup.y);
-    const Junctions &VSegLo = Vert(klo.x);
-    const Junctions &VSegUp = Vert(kup.x);
-#endif
+    //fprintf( stderr, "\ttracer @(%g,%g)\n", tracer->vertex.x, tracer->vertex.y);
+    //--------------------------------------------------------------------------
+    // find next
+    //--------------------------------------------------------------------------
+    const Junction *jnext = 0;
+    {
+        const Tracer *p  = tracer;
+        if( p->jnext )
+        {
+            jnext = p->jnext;
+        }
+        else
+        {
+            for(p=p->next;p!=tracer;p=p->next)
+            {
+                if(p->jprev)
+                {
+                    jnext = p->jprev;
+                    break;
+                }
+                
+                if(p->jnext)
+                {
+                    jnext = p->jnext;
+                    break;
+                }
+            }
+        }
+       
+    }
+    
+    if(!jnext)
+    {
+        fprintf( stderr, "can't find jnext!\n");
+    }
+    //fprintf( stderr, "\t\t: jnext@(%g,%g)\n", jnext->vertex.x, jnext->vertex.y);
+    
+    //--------------------------------------------------------------------------
+    // find prev
+    //--------------------------------------------------------------------------
+    const Junction *jprev = 0;
+    {
+        const Tracer *p = tracer;
+        if( p->jprev )
+        {
+            jprev = p->jprev;
+        }
+        else
+        {
+            for(p=p->prev;p!=tracer;p=p->prev)
+            {
+                if(p->jnext)
+                {
+                    jprev = p->jnext;
+                    break;
+                }
+                
+                if(p->jprev)
+                {
+                    jprev = p->jprev;
+                    break;
+                }
+            }
+        }
+    }
+    if(!jprev)
+    {
+        fprintf( stderr, "can't find jprev!");
+    }
+    //fprintf( stderr, "\t\t: jprev@(%g,%g)\n", jprev->vertex.x, jprev->vertex.y);
+
+    assert(jnext!=jprev);
     
     
 }
