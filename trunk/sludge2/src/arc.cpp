58fe3bf65c58df94af8560a3e4b9a1355479f513
yann.bouret@gmail.com <yann.bouret@gmail.com@cb0b72a0-0d06-b54d-855f-8f256b0ebc6e>
2012-10-30 11:04:04 +0000

    almost won, but finally lost

--- trunk/sludge2/src/arc.cpp
+++ trunk/sludge2/src/arc.cpp
@@ -4,6 +4,8 @@
 Arc:: ~Arc() throw() {}
 
 Arc:: Arc() throw() :
+Func( this, & Arc::func),
+Grad( this, & Arc::grad),
 a(),
 b(),
 c(),
@@ -13,7 +15,9 @@ C0(0),
 r1(),
 t1(),
 C1(0),
-delta_r()
+delta_r(),
+delta(0),
+delta2(0)
 {
 }
 
@@ -22,6 +26,120 @@ Vertex Arc:: operator()( const Real mu ) const throw()
 {
     return r0 + mu * a + (mu*mu) * b + (mu*mu*mu) * c;
 }
+
+#include "yocto/code/ipower.hpp"
+
+Real Arc:: func( const array<Real> &U ) const
+{
+    Real ans = 0;
+    load(U);
+    
+    {
+        const Vertex dpos = ( (a+b+c) - delta_r)/delta;
+        ans += 0.5 * dpos.norm2();
+    }
+    const Vertex dr0  = a;
+    const Real   dr0n = dr0.norm();
+    {
+        ans += (1.0 - (t0*dr0)/dr0n);
+    }
+    
+    const Vertex dr1  = a+2.0*b+3.0*c;
+    const Real   dr1n = dr1.norm();
+    {
+        
+        ans += (1.0 - (t1*dr1)/dr1n);
+    } 
+    
+    {
+        const Real arg = Vertex::det(a,b)/ipower(dr0n,3) - 0.5*C0;
+        ans += 0.5 * delta2 * arg * arg;
+    }
+    
+    {
+        const Real arg = (Vertex::det(a,b)+3*Vertex::det(a,c)+3*Vertex::det(b,c))/ipower(dr1n,3) - 0.5*C1;
+        ans += 0.5 * delta2 * arg * arg;
+    }
+    return ans;
+}
+
+
+void Arc:: grad( array<Real> &G, const array<Real> &U) const
+{
+    assert( G.size() == U.size() );
+    for( size_t i=G.size();i>0;--i) G[i] = 0;
+    
+    
+    {
+        const Vertex g = ( (a+b+c) - delta_r)/delta2;
+        G[1] += g.x;
+        G[2] += g.y;
+        G[3] += g.x;
+        G[4] += g.y;
+        G[5] += g.x;
+        G[6] += g.y;
+    }
+    
+    const Vertex dr0   = a;
+    const Real   dr0n  = dr0.norm();
+    const Real   dr0n3 = dr0n*dr0n*dr0n;
+    {
+        const Real   gs    = dr0 * t0;
+        const Real   gx    = dr0.x * gs / dr0n3 - t0.x/dr0n;
+        const Real   gy    = dr0.y * gs / dr0n3 - t0.y/dr0n;
+        G[1] += gx;
+        G[2] += gy;
+    }
+    
+    const Vertex dr1   = a+2.0*b+3.0*c;
+    const Real   dr1n  = dr1.norm();
+    const Real   dr1n3 = dr1n*dr1n*dr1n;
+    
+    {
+        const Real   gs    = dr1 * t1;
+        const Real   gx    = dr1.x * gs / dr1n3 - t1.x/dr1n;
+        const Real   gy    = dr1.y * gs / dr1n3 - t1.y/dr1n;
+        G[1] += gx;
+        G[2] += gy;
+        G[3] += 2*gx;
+        G[4] += 2*gy;
+        G[5] += 3*gx;
+        G[6] += 3*gy;
+    }
+    
+    {
+        const Real detAB = Vertex::det(a,b);
+        const Real arg   = delta2 * (detAB/dr0n3 - 0.5*C0);
+        const Real dr0n5 = dr0n3 * dr0n * dr0n;
+        G[1] += arg * ( b.y / dr0n3 - 3*a.x*detAB/dr0n5);
+        G[2] += arg * (-b.x / dr0n3 - 3*a.y*detAB/dr0n5);
+        G[3] += arg * (-a.y / dr0n3 );
+        G[4] += arg * ( a.x / dr0n3  );
+    }
+
+    {
+        const Real detAB = Vertex::det(a,b);
+        const Real detBC = Vertex::det(b,c);
+        const Real detAC = Vertex::det(a,c);
+        const Real sdet  = detAB + 3*(detBC+detAC);
+        const Real dr1n5 = dr1n3 * dr1n * dr1n;
+        const Real arg   = delta2 * ( sdet/dr1n3 - 0.5*C1);
+        const Real fac_x = 3*dr1.x*sdet/dr1n5;
+        const Real fac_y = 3*dr1.y*sdet/dr1n5;
+
+        G[1] += arg * ( ( 3*c.y +   b.y )/dr1n3 -     fac_x);
+        G[2] += arg * ( (-3*c.x -   b.x )/dr1n3 -     fac_y);
+        
+        G[3] += arg * ( ( 3*c.y -   a.y )/dr1n3 - 2 * fac_x);
+        G[4] += arg * ( (   a.x - 3*c.x )/dr1n3 - 2 * fac_y);
+
+        G[5] += arg * ( (-3*b.y - 3*a.y )/dr1n3 - 3 * fac_x);
+        G[6] += arg * ( ( 3*b.x + 3*a.x )/dr1n3 - 3 * fac_y);
+        
+    }
+    
+}
+
 
 #define ARC_NVAR 6
 
@@ -39,13 +157,24 @@ void Arc:: load( const array<Real> &U ) const
 void Arc:: init(array<Real> &U) const
 {
     delta_r = r1 - r0;
-    const Real scale = 0.1 * delta_r.norm();
+    delta2  = delta_r.norm2();
+    delta   = Sqrt(delta2);
+    
+    const Real scale = 0.1 * delta;
     a = scale * t0;
+    
+#if 0
+    // 3 params
     const Vertex v1 = delta_r - a;
     const Vertex v2 = scale * t1 - a;
     
     c = v2 - 2.0*v1;
-    b = v1 - c;
+    b = 3.0*v1 - v2;
+#else
+    // 2 params
+    c.ldz();
+    b = 0.5 * (scale*t1 - a );
+#endif
     
     U[1] = a.x;
     U[2] = a.y;
@@ -56,99 +185,29 @@ void Arc:: init(array<Real> &U) const
     std::cerr << "init=" << U << std::endl;
 }
 
-#include "yocto/code/ipower.hpp"
 
-void Arc:: estimate( matrix<Real> &P, array<Real> &F, const array<Real> &U ) const
+bool Arc:: disp( const array<Real> &U ) const
 {
-    load(U);
+    std::cerr << "F(" << U << ")=" << Func(U) << std::endl;
     
-    {
-        F[1] = a.x+b.x+c.x - delta_r.x;
-        array<Real> &J = P[1];
-        J[1] = 1;
-        J[2] = 0;
-        J[3] = 1;
-        J[4] = 0;
-        J[5] = 1;
-        J[6] = 0;
-    }
+    vector<Real> G(U.size(),0);
+    Grad(G,U);
+    std::cerr << "Grad=" << G << std::endl;
     
-    {
-        F[2] = a.y+b.y+c.y - delta_r.y;
-        array<Real> &J = P[2];
-        J[1] = 0;
-        J[2] = 1;
-        J[3] = 0;
-        J[4] = 1;
-        J[5] = 0;
-        J[6] = 1;
-    }
-    
-    const Vertex dr0      = a;
-    const Real   dr0_norm = dr0.norm();
-    {
-        F[3] = dr0 * t0 - dr0_norm;
-        array<Real> &J = P[3];
-        J[1] = t0.x - dr0.x / dr0_norm;
-        J[2] = t0.y - dr0.y / dr0_norm;
-        J[3] = J[4] = J[5] = J[6] = 0;
-    }
-    
-    const Vertex dr1 = a + (b+b) + (c+c+c);
-    const Real   dr1_norm = dr1.norm();
-    {
-        F[4] = dr1 * t1 - dr1_norm;
-        array<Real> &J = P[4];
-        J[1] = t1.x - dr1.x / dr1_norm;
-        J[2] = t1.y - dr1.y / dr1_norm;
-        J[3] = 2 * J[1];
-        J[4] = 2 * J[2];
-        J[5] = 3 * J[1];
-        J[6] = 3 * J[2];
-    }
-    
-    {
-        F[5] = Vertex::det(a,b) - 0.5 * C0 * ipower(dr0_norm,3);
-        array<Real> &J = P[5];
-        const Real fac5 = 1.5 * C0 * dr0_norm;
-        J[1] =  b.y -  a.x * fac5;
-        J[2] = -b.x -  a.y * fac5;
-        J[3] = -a.y;
-        J[4] =  a.x;
-        J[5] = 0;
-        J[6] = 0;
-    }
-    
-    {
-        F[5] = Vertex::det(a,b) + 3 * Vertex::det(a,c) + 3 * Vertex::det(b,c) - 0.5 * C1 * ipower(dr1_norm,3);
-        array<Real> &J = P[6];
-        const Real fac6 = 1.5 * C1* dr1_norm;
-        J[1] =  3 * c.y +     b.y - fac6 * dr1.x;
-        J[2] = -3 * c.x -     b.x - fac6 * dr1.y;
-        J[3] =  3 * c.y -     a.y - 2 * fac6 * dr1.x;
-        J[4] = -3 * c.x +     a.x - 2 * fac6 * dr1.y;
-        J[5] = -3 * b.y - 3 * a.y - 3 * fac6 * dr1.x;
-        J[6] =  3 * b.x + 3 * a.x - 3 * fac6 * dr1.y;
-    }
+    return true;
 }
-
 
 
 ArcSolver:: ~ArcSolver() throw() {}
 
 ArcSolver:: ArcSolver() :
-P(ARC_NVAR,ARC_NVAR),
-U(ARC_NVAR,0.0),
-F(ARC_NVAR,0.0),
-h(ARC_NVAR,0.0),
-ls(ARC_NVAR)
+U(ARC_NVAR,0.0)
 {
-    
     
 }
 
 #include "yocto/math/kernel/algebra.hpp"
-#include "yocto/math/kernel/svd.hpp"
+#include "yocto/math/opt/cgrad.hpp"
 
 void ArcSolver:: compute(const Arc &arc)
 {
@@ -157,32 +216,12 @@ void ArcSolver:: compute(const Arc &arc)
     //--------------------------------------------------------------------------
     arc.init(U);
     
-    for(size_t iter=0; iter<30;++iter)
-    {
-        //-- compute jacobian and value
-        arc.estimate(P, F, U);
-        std::cerr << "U=" << U << std::endl;
-        std::cerr << "P=" << P << std::endl;
-        std::cerr << "F=" << F << std::endl;
-        
-        vector<Real> W(ARC_NVAR,0);
-        matrix<Real> V(ARC_NVAR,ARC_NVAR);
-        if( !svd<Real>::build(P, W, V) )
-        {
-            throw exception("Singular SVD arc");
-        }
-        //std::cerr << "W=" << W << std::endl;
-        //std::cerr << "M=" << P << std::endl;
-        svd<Real>::truncate(W,1e-3);
-        std::cerr << "W=" << W << std::endl;
-        //std::cerr << "V=" << V << std::endl;
-        svd<Real>::solve(P, W, V, F, h);
-        std::cerr << "h=" << h << std::endl;
-        
-        algebra<Real>::sub(U, h);
-        
-    }
+    cgrad<Real>::callback cb( &arc, &Arc::disp);
+    cgrad<Real>::optimize(arc.Func, arc.Grad, U, 1e-5, &cb);
     
     arc.load(U);
+    std::cerr << "a=" << arc.a << std::endl;
+    std::cerr << "b=" << arc.b << std::endl;
+    std::cerr << "c=" << arc.c << std::endl;
 }
 
