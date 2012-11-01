aece1d659debc338f3df7ca3a3c4ed05fc0f1345
yann.bouret@gmail.com <yann.bouret@gmail.com@cb0b72a0-0d06-b54d-855f-8f256b0ebc6e>
2012-11-01 10:45:18 +0000

    works with degree 2 arc

--- sludge2/src/tests/test-arc.cpp
+++ sludge2/src/tests/test-arc.cpp
@@ -9,13 +9,13 @@ YOCTO_UNIT_TEST_IMPL(arc)
     Arc arc;
     
     arc.r0 = Vertex(-1,1);
-    arc.t0 = Vertex(1,1);
+    arc.t0 = Vertex(1,0.5);
     arc.t0.normalize();
-    arc.C0 = -1;
+    arc.C0 = 0;
     
     
     arc.r1 = Vertex(1,1);
-    arc.t1 = Vertex(1,-0.5);
+    arc.t1 = Vertex(-1,-1);
     arc.t1.normalize();
     arc.C1 = 0;
     
