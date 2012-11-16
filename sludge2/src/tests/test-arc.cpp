562de9298771f3d4d5bd928707f3a9e445e92d9c
yann.bouret@gmail.com <yann.bouret@gmail.com@cb0b72a0-0d06-b54d-855f-8f256b0ebc6e>
2012-11-16 08:16:54 +0000

    testing 1D propagation

--- sludge2/src/tests/test-arc.cpp
+++ sludge2/src/tests/test-arc.cpp
@@ -1,12 +1,11 @@
 #include "yocto/utest/run.hpp"
 #include "../arc.hpp"
-
-
+#include "yocto/math/kernel/svd.hpp"
+#include "yocto/string/conv.hpp"
 
 YOCTO_UNIT_TEST_IMPL(arc)
 {
-       
-
+    
     
 }
 YOCTO_UNIT_TEST_DONE()
