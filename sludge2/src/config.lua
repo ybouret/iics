71de386281fc7407c90a41a777b9e0d71f9c6b51
yann.bouret@gmail.com <yann.bouret@gmail.com@cb0b72a0-0d06-b54d-855f-8f256b0ebc6e>
2012-12-14 16:00:28 +0000

    added Nx,Ny in config

--- sludge2/src/config.lua
+++ sludge2/src/config.lua
@@ -3,4 +3,7 @@ right_wall     = true; -- overrides right_pressure
 --right_wall     = false;
 gamma = 0.05
 
-ftol=1e-2
+ftol=1e-5
+
+Nx=30;
+Ny=50;
--- sludge2/src/visit/sim/init.cpp
+++ sludge2/src/visit/sim/init.cpp
@@ -15,13 +15,14 @@ void Simulation:: initialize()
             bubble->set_pressure(1.0);
         }
         
-        if(false)
+        if(true)
         {
             const Vertex center(3*full_length.x/4,full_length.y/4);
             const Real   radius(full_length.x/9);
             Bubble *bubble = bubbles.append();
-            bubble->map_peanut(center, radius,0.8);
+            bubble->map_peanut(center, radius,0.85);
             bubble->set_pressure(1.0);
+            bubble->rotate(M_PI/4);
         }
         
 #if 0
