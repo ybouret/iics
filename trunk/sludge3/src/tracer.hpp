63aa0c0a098bb79e065b91027df1b92ceaa16ff4
yann.bouret@gmail.com <yann.bouret@gmail.com@cb0b72a0-0d06-b54d-855f-8f256b0ebc6e>
2013-05-27 13:27:59 +0000

    bubbles, need to test MPI

--- trunk/sludge3/src/tracer.hpp
+++ trunk/sludge3/src/tracer.hpp
@@ -7,26 +7,29 @@
 class Tracer : public object
 {
 public:
+    static const int Tag;
+    
     Tracer *prev;
     Tracer *next;
     
-    Vertex pos;  //!< current pos
-    Vertex edge; //!< to next
-    Real   dist; //!< to next
+    Vertex pos;  //!< current pos +2 Real
+    Vertex edge; //!< to next     +2 Real
+    Real   dist; //!< to next     +1 Real
     
+    static const size_t NumReals = 5;
     
     explicit Tracer() throw();
     explicit Tracer( const Vertex v ) throw();
     virtual ~Tracer() throw();
     
-    void hash( Hasher &h ) const throw();
+    void hash_tracer( Hasher &h ) const throw();
     
     class Ring : public core::clist_of<Tracer>
     {
     public:
         explicit Ring() throw();
         virtual ~Ring() throw();
-        void hash( Hasher &h) const throw();
+        void hash_ring( Hasher &h) const throw();
         
     private:
         YOCTO_DISABLE_COPY_AND_ASSIGN(Ring);
