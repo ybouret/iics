5b7f678be9889a5a3e710a72632351999ffdd77d
yann.bouret@gmail.com <yann.bouret@gmail.com@cb0b72a0-0d06-b54d-855f-8f256b0ebc6e>
2013-05-28 12:41:25 +0000

    advanced shapes

--- sludge3/src/shape.hpp
+++ sludge3/src/shape.hpp
@@ -5,7 +5,9 @@
 
 struct Shape
 {
-	static void Circle( Bubble *b, const Vertex center, Real radius ) throw();
+	static void Circle( Bubble *b, const Vertex center, Real radius );
+    static void Ellipse( Bubble *b, const Vertex C, const Vertex R);
+    static void Rotate( Bubble *b, const Real alpha);
 };
 
 #endif
