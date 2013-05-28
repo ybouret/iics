43951d8f724644f64f8a2a66923d9315e99af5d2
yann.bouret@gmail.com <yann.bouret@gmail.com@cb0b72a0-0d06-b54d-855f-8f256b0ebc6e>
2013-05-28 08:02:15 +0000

    tracer/grid location

--- trunk/sludge3/src/grid.hpp
+++ trunk/sludge3/src/grid.hpp
@@ -15,14 +15,30 @@ typedef rmesh<Layout,Real>   Grid;
 typedef array1D<Real>        Array1D;
 typedef region2D<Real>::type Region2D;
 
+#define SLUDGE_INSIDE (0)
+#define SLUDGE_TOP    (1)
+#define SLUDGE_BOTTOM (2)
+#define SLUDGE_LEFT   (4)
+#define SLUDGE_RIGHT  (8)
 
 struct __Grid
 {
+    
     //! save to gnuplot format
     static void SaveDat( const Grid &grid, const string &fn );
     
     //! Get lambda for bubble resolution
-    static Real ComputeLambda( const Grid &grid);
+    static Real ComputeLambda( const Grid &grid) throw();
+    
+    //! locate point on grid
+    /**
+     \param p any point
+     \param lo lower coordinates such 
+     that X[lo.x] <= p.x < X[lo.x+1]
+     and  Y[lo.y] <= p.y < Y[lo.y+1]
+     \return status
+     */
+    static int Locate( const Grid &grid, const Vertex &p, Coord &lo) throw();
 };
 
 #endif
