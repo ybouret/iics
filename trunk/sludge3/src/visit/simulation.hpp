8eb567bada9faa94de8266a34e1216f9d51c86f7
yann.bouret@gmail.com <yann.bouret@gmail.com@cb0b72a0-0d06-b54d-855f-8f256b0ebc6e>
2013-05-31 11:20:21 +0000

    minimal VisIt simulation, no meta/data

---
+++ trunk/sludge3/src/visit/simulation.hpp
@@ -0,0 +1,27 @@
+#ifndef SLUDGE_VISIT_SIMULATION_INCLUDED
+#define SLUDGE_VISIT_SIMULATION_INCLUDED 1
+
+#include "../mpi/workspace.hpp"
+#include "yocto/spade/visit.hpp"
+#include "yocto/visit/interface.hpp"
+
+
+class Simulation :
+public Workspace,
+public VisIt::Simulation,
+public VisItIO
+{
+public:
+    explicit Simulation(const mpi   &ref,
+                        const Coord  N,
+                        const Vertex Q
+                        );
+    virtual ~Simulation() throw();
+    
+    
+private:
+    YOCTO_DISABLE_COPY_AND_ASSIGN(Simulation);
+};
+
+#endif
+
