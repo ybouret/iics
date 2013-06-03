ed0b3aaf81ecafea80198bc30633e3aca6ff68fa
yann.bouret@gmail.com <yann.bouret@gmail.com@cb0b72a0-0d06-b54d-855f-8f256b0ebc6e>
2013-06-03 09:38:21 +0000

    need to check auto_countour, too many points are created

--- trunk/sludge3/src/visit/simulation.cpp
+++ trunk/sludge3/src/visit/simulation.cpp
@@ -38,16 +38,30 @@ void Simulation:: get_meta_data(visit_handle &md) const
     
     //! append B on grid
     VisIt_SimulationMetaData_addVariable(md, variable_meta_data<Real>("B", "grid"));
-
+    
     //! append gradP on grid
     VisIt_SimulationMetaData_addVariable(md, variable_meta_data<Vertex>("gradP", "grid"));
-
+    
     //! append Enter on grid
     VisIt_SimulationMetaData_addVariable(md, variable_meta_data<Vertex>("Enter", "grid"));
-
+    
     //! append Leave on grid
     VisIt_SimulationMetaData_addVariable(md, variable_meta_data<Vertex>("Leave", "grid"));
-
+    
+    //! append bubbles
+    for( const Bubble *b = bubbles.head;b;b=b->next)
+    {
+        const string bn = vformat("bubble%u", unsigned(b->UID));
+        visit_handle h = VISIT_INVALID_HANDLE;
+        if(VisIt_CurveMetaData_alloc(&h) == VISIT_OKAY )
+        {
+            VisIt_CurveMetaData_setName(h, bn.c_str());
+            //VisIt_CurveMetaData_setXLabel(cmd, "Angle");
+            ////VisIt_CurveMetaData_setXUnits(cmd, "radians");
+            //VisIt_CurveMetaData_setYLabel(cmd, "Amplitude");
+            VisIt_SimulationMetaData_addCurve(md, h);
+        }
+    }
     
 }
 
@@ -155,12 +169,47 @@ visit_handle Simulation:: get_variable( int domain, const string &name ) const
             VisIt_VariableData_setDataD(h, VISIT_OWNER_SIM, nComponents, nTuples, (Real *)(Leave.entry));
         }
     }
-
+    
     
     
     return h;
-
+    
 }
+
+visit_handle Simulation:: get_curve( const string &name ) const
+{
+    visit_handle h = VISIT_INVALID_HANDLE;
+    
+    for( const Bubble *b = bubbles.head;b;b=b->next)
+    {
+        const string bn = vformat("bubble%u", unsigned(b->UID));
+        if( bn == name )
+        {
+            if(VisIt_CurveData_alloc(&h) != VISIT_ERROR)
+            {
+                visit_handle hxc, hyc;
+                VisIt_VariableData_alloc(&hxc);
+                VisIt_VariableData_alloc(&hyc);
+                
+                const size_t np = b->size+1;
+                vector<Real> bx(np,0);
+                vector<Real> by(np,0);
+                const Tracer *tr = b->root;
+                for(size_t i=1;i<=np;++i,tr=tr->next)
+                {
+                    bx[i] = tr->pos.x;
+                    by[i] = tr->pos.y;
+                }
+                VisIt_VariableData_setDataD(hxc,VISIT_OWNER_COPY,1,np,&bx[1]);
+                VisIt_VariableData_setDataD(hyc,VISIT_OWNER_COPY,1,np,&by[1]);
+                VisIt_CurveData_setCoordsXY(h, hxc, hyc);
+            }
+        }
+    }
+    return h;
+    
+}
+
 
 ////////////////////////////////////////////////////////////////////////////////
 //
