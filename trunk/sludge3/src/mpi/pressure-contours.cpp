9bf4056d2f6dba559ecffd41e59af6262973cfc2
yann.bouret@gmail.com <yann.bouret@gmail.com@cb0b72a0-0d06-b54d-855f-8f256b0ebc6e>
2013-06-03 15:42:16 +0000

    Leave/Enter WIP

--- trunk/sludge3/src/mpi/pressure-contours.cpp
+++ trunk/sludge3/src/mpi/pressure-contours.cpp
@@ -1,5 +1,23 @@
 #include "workspace.hpp"
 
+static 
+const Junction * __find_next_horz( const Junction::List &JL, const Real x)
+{
+    assert(JL.size>0);
+    bool  in_bubble   = true;
+    const Junction *J = JL.head;
+    const Junction *K = J->next;
+    while(K)
+    {
+        if(J->value>x)
+            return J;
+        in_bubble = !in_bubble;
+        J=K;
+        K=K->next;
+    }
+    throw exception("missing horizontal junction");
+}
+
 void Workspace:: pressurize_contours()
 {
     Enter.ldz();
@@ -7,7 +25,7 @@ void Workspace:: pressurize_contours()
     //==========================================================================
     // using horizontal axis => x components of Leave/Enter
     //==========================================================================
-    for(unit_t j=outline.lower.y;j<=outline.upper.y;++j)
+    for(unit_t j=bulk_jmin;j<=bulk_jmax;++j)
     {
         const Junction::List &JL = junctions.Horz(j);
         
@@ -17,6 +35,28 @@ void Workspace:: pressurize_contours()
             //------------------------------------------------------------------
             // special case: left side
             //------------------------------------------------------------------
+            assert(B[j][lower.x]<0);
+            if(B[j][bulk_imin]>=0)
+            {
+                Enter[j][bulk_imin].x = 0;
+            }
+            
+            //------------------------------------------------------------------
+            // bulk case
+            //------------------------------------------------------------------
+            for( unit_t i=bulk_imin;i<=bulk_imax;++i)
+            {
+                if(B[j][i]<0)
+                {
+                    const Real      Xi    = X[i];
+                    const Junction *Jnext = B[j][i+1] >= 0 ? __find_next_horz(JL, Xi) : 0;
+                    if(Jnext)
+                    {
+                        
+                    }
+                }
+            }
+            
             
             
         }
