5b5c741ecf1e365ee813bd45f18729eaa4d6e2e4
yann.bouret@gmail.com <yann.bouret@gmail.com@cb0b72a0-0d06-b54d-855f-8f256b0ebc6e>
2013-06-04 08:12:44 +0000

    pressure fields OK in bulk, need to solve pressure

--- sludge3/src/visit/sim-init.cpp
+++ sludge3/src/visit/sim-init.cpp
@@ -15,7 +15,7 @@ void Simulation:: init_one_bubble()
             Bubble *b = bubbles.append();
             Shape::Blob(b, center, radius, 0.4 + 0.55 * alea<Real>(), 0.1 + 0.8 * alea<Real>() );
             //std::cerr << "radius=" << radius << std::endl;
-            //Shape::Circle(b, center, radius);
+            Shape::Circle(b, center, radius);
             Shape::Rotate(b, numeric<Real>::two_pi * alea<Real>() );
         }
     }
