b3bb043d804d7c9e9b9995ea3858b0a4d5480c54
yann.bouret@gmail.com <yann.bouret@gmail.com@cb0b72a0-0d06-b54d-855f-8f256b0ebc6e>
2013-06-04 07:37:34 +0000

    99% horizontal code

--- trunk/sludge3/src/mpi/pressure-contours.cpp
+++ trunk/sludge3/src/mpi/pressure-contours.cpp
@@ -41,13 +41,15 @@ void Workspace:: pressurize_contours()
                     int             status = BUBBLE_NONE;
                     const Junction *before = 0;
                     const Junction *after  = 0;
-                    
+                    Real            phi    = 0;
+                    Real            psi    = 0;
                     if(B[j][i-1]>=0)
                     {
                         status |= BUBBLE_BEFORE;
                         before  = JL.before(Xi);
                         if(!before)
                             throw exception("@y=%g: no junction before x=%g", Y[j], Xi);
+                        psi = Xi - before->value;
                     }
                     
                     if(B[j][i+1]>=0)
@@ -56,20 +58,37 @@ void Workspace:: pressurize_contours()
                         after   = JL.after(Xi);
                         if(!after)
                             throw exception("@y=%g: no junction after x=%g", Y[j], Xi);
+                        phi = after->value - Xi;
                     }
                     
+                    //----------------------------------------------------------
+                    // handle topology
+                    //----------------------------------------------------------
                     switch(status)
                     {
                         case BUBBLE_BEFORE:
-                            
+                        {
+                            const Real Pp = P[j][i+1];
+                            const Real Pb = before->pressure;
+                            Leave[j][i-1].x = Pp - (2*delta.x) * (Pp - Pb)/(delta.x+psi);
+                        }
                             break;
                             
                         case BUBBLE_AFTER:
-                            
+                        {
+                            const Real Pm   = P[j][i-1];
+                            const Real Pb   = after->pressure;
+                            Enter[j][i+1].x = Pm + (2*delta.x) * (Pb - Pm) / (delta.x + phi);
+                        }
                             break;
                             
                         case BUBBLE_BOTH:
+                        {
+                            const Real fac = (2*delta.x)/(phi+psi);
+                            Enter[j][i+1].x = after->pressure  * fac;
+                            Leave[j][i-1].x = before->pressure * fac;
                             
+                        }
                             break;
                             
                         default:
@@ -81,12 +100,28 @@ void Workspace:: pressurize_contours()
             
             //------------------------------------------------------------------
             //
-            // on side(s)
+            // on Horizonal sideside(s)
             //
             //------------------------------------------------------------------
         }
         
     }
+    
+    
+    //==========================================================================
+    //
+    // using Vertical axis => y components of Leave/Enter
+    //
+    //==========================================================================
+    for(unit_t i=outline.lower.x; i <= outline.upper.x; ++i )
+    {
+        const Junction::List &JL = junctions.Vert(i);
+        if(JL.size)
+        {
+            
+        }
+    }
+    
     
 }
 
