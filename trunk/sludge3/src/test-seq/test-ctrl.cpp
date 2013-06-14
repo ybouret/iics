f253c96fdb7766b6e19c9d936c5561c16d6d9b9e
yann.bouret@gmail.com <yann.bouret@gmail.com@cb0b72a0-0d06-b54d-855f-8f256b0ebc6e>
2013-06-14 14:31:31 +0000

    new control algorithm

--- trunk/sludge3/src/test-seq/test-ctrl.cpp
+++ trunk/sludge3/src/test-seq/test-ctrl.cpp
@@ -3,34 +3,34 @@
 #include "shape.hpp"
 
 
+
+static inline
+void check( Bubble *b, const char *pfx )
+{
+    b->save_dat( vformat("%s0.dat", pfx ) );
+    b->adjust_contour();
+    
+    
+}
+
 YOCTO_UNIT_TEST_IMPL(ctrl)
 {
     Real   lam = 0.2;
-    Real   mu  = 0.15;
+    //Real   mu  = 0.15;
     Real   gam = 1;
     Bubble bubble(lam,gam,1);
     
     std::cerr << "Circle" << std::endl;
     Shape::Circle(&bubble, Vertex(0,0), 4);
-    bubble.save_dat( "c0.dat");
-    
-    bubble.reduce(mu);
-    bubble.save_dat( "c1.dat");
+    check(&bubble, "c");
     
     
-    std::cerr << "Ellipse" << std::endl;
+    
     Shape::Ellipse(&bubble, Vertex(0,0), Vertex(5,2));
-    bubble.save_dat( "e0.dat");
+    check(&bubble,"e");
     
-    bubble.reduce(mu);
-    bubble.save_dat( "e1.dat");
-    
-    std::cerr << "Blob" << std::endl;
     Shape::Blob(&bubble, Vertex(0,0), 4, 0.5, 0.0);
-    bubble.save_dat( "b0.dat");
-    
-    for(size_t i=0;i<10;++i)
-        bubble.reduce1(mu);
-    bubble.save_dat( "b1.dat");
+    check(&bubble,"b");
+
 }
 YOCTO_UNIT_TEST_DONE()
