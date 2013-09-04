9c9f8a9f9a34779e476d1480d284388e6be3a965
yann.bouret@gmail.com <yann.bouret@gmail.com@cb0b72a0-0d06-b54d-855f-8f256b0ebc6e>
2013-09-04 09:55:24 +0000

    preparing to load simulation output

--- md-bubbles/src/find-bubbles.cpp
+++ md-bubbles/src/find-bubbles.cpp
@@ -5,6 +5,7 @@
 #include "yocto/exception.hpp"
 #include "yocto/string/conv.hpp"
 #include "yocto/sequence/vector.hpp"
+#include "yocto/math/alg/delaunay.hpp"
 
 #include <iostream>
 
@@ -17,25 +18,35 @@ typedef v3d<Real> V3D;
 class Atom
 {
 public:
-    V3D r;
+    V3D  r;
+    Real voronoi;
     
     static inline bool IsSep(const char C) throw() { return C == ' ' || C == '\t'; }
     
-    inline  Atom() throw() : r() {}
+    inline  Atom() throw() : r(), voronoi(0) {}
     inline ~Atom() throw() {}
     
     inline Atom(const Atom &atom ) throw() :
-    r( atom.r )
+    r( atom.r ),
+    voronoi( atom.voronoi )
     {
     }
     
+    template <typename T>
     static inline
-    Real NextReal( tokenizer &tkn, unsigned iline, const char *id )
+    T Next( tokenizer &tkn, unsigned iline, const char *id )
     {
         assert(id);
         if( !tkn.get_next(IsSep) ) throw exception("%u: missing %s", iline, id);
         const string tmp = tkn.to_string();
-        return strconv::to<Real>(tmp,id);
+        return strconv::to<T>(tmp,id);
+    }
+    
+    static inline
+    void SkipNext( tokenizer &tkn, unsigned iline, const char *id )
+    {
+        assert(id);
+        if( !tkn.get_next(IsSep) ) throw exception("%u: missing field %s", iline, id);
     }
     
     inline void parse( const string &line, unsigned iline)
@@ -45,9 +56,14 @@ public:
         //======================================================================
         // read position
         //======================================================================
-        r.x = NextReal(tkn, iline, "x");
-        r.y = NextReal(tkn, iline, "y");
-        r.z = NextReal(tkn, iline, "z");
+        r.x = Next<Real>(tkn, iline, "x");
+        r.y = Next<Real>(tkn, iline, "y");
+        r.z = Next<Real>(tkn, iline, "z");
+        
+        //======================================================================
+        // read voronoi
+        //======================================================================
+        voronoi = Next<Real>(tkn, iline, "voronoi");
         
     }
     
@@ -56,16 +72,49 @@ private:
 };
 
 typedef vector<Atom> Atoms;
+typedef vector<V3D>  Vertices;
+typedef vector<iTriangle> Triangles;
 
 class Frame : public Atoms
 {
 public:
-    explicit Frame() throw() : Atoms()
+    Frame *next;
+    Frame *prev;
+    explicit Frame() throw() :
+    Atoms(),
+    next(0),
+    prev(0)
     {
+    }
+    
+    void find_gas(Vertices &gas, Real vmin ) const
+    {
+        gas.free();
+        for(size_t j=size();j>0;--j)
+        {
+            const Atom &atom = (*this)[j];
+            if(atom.voronoi>=vmin)
+                gas.push_back( atom.r );
+        }
+    }
+    
+    static
+    void Triangulate( Triangles &triangles, const Vertices &gas )
+    {
+        delaunay<Real>::build(triangles, gas);
     }
     
     virtual ~Frame() throw() {}
     
+    static Frame *LoadNext(ios::istream &fp, unsigned iline)
+    {
+        string line;
+        if(fp.read_line(line)<0)
+            return 0;
+        
+        
+        return 0;
+    }
     
 private:
     YOCTO_DISABLE_COPY_AND_ASSIGN(Frame);
