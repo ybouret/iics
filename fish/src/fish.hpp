fb63e6ee3d0d7b83da77a329a4cc6eb1d5b8006c
ybouret <yann.bouret@gmail.com>
2015-04-10 12:08:11 +0200

    starting a fish makeup

---
+++ fish/src/fish.hpp
@@ -0,0 +1,9 @@
+#ifndef FISH_INCLUDED
+#define FISH_INCLUDED 1
+
+#include "yocto/math/dat/b-splines.hpp"
+
+
+
+#endif
+
---
+++ fish/src/genfish.cpp
@@ -0,0 +1,8 @@
+#include "fish.hpp"
+#include "yocto/program.hpp"
+
+YOCTO_PROGRAM_START()
+{
+    
+}
+YOCTO_PROGRAM_END()
