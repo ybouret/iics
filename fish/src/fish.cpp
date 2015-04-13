1ff759d8ea8a2ecd79e6f76cbf3f447550f93f1d
ybouret <yann.bouret@gmail.com>
2015-04-13 08:46:21 +0200

    start building fish build-up

--- fish/src/fish.cpp
+++ fish/src/fish.cpp
@@ -1,2 +1,89 @@
 #include "fish.hpp"
 
+////////////////////////////////////////////////////////////////////////////////
+//
+//
+//
+////////////////////////////////////////////////////////////////////////////////
+
+Point::  Point() throw() : i(Index++), r() {}
+Point:: ~Point() throw() {}
+
+size_t Point::Index = 0;
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//
+//
+////////////////////////////////////////////////////////////////////////////////
+Slice:: ~Slice() throw() {}
+
+Slice:: Slice(double zz,double pr) throw() :
+z(zz),
+perimeter(pr),
+points()
+{
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//
+//
+////////////////////////////////////////////////////////////////////////////////
+Triangle:: ~Triangle() throw() {}
+
+
+Triangle:: Triangle(const pPoint &A,
+                    const pPoint &B,
+                    const pPoint &C) throw():
+a(A),b(B),c(C),
+G( (1.0/3) * (a->r+b->r+c->r) ),
+n(),
+S(0)
+{
+    assert(a->i!=b->i);
+    assert(a->i!=c->i);
+    assert(b->i!=c->i);
+    {
+        //const vtx_t G = (1.0/3) * (a->r+b->r+c->r);
+        const vtx_t AB(a->r,b->r);
+        const vtx_t AC(a->r,c->r);
+        const vtx_t NN = vtx_t::cross_(AB, AC);
+        const vtx_t middle(0,0,0.5);
+        const vtx_t Q(middle,G);
+        if( (NN*Q) <= 0 )
+        {
+            b.swap_with(c);
+        }
+    }
+
+    {
+        const vtx_t AB(a->r,b->r);
+        const vtx_t AC(a->r,c->r);
+        n = vtx_t::cross_(AB, AC);
+        S = n.norm();
+        n.normalize();
+    }
+}
+
+
+Triangle:: Triangle(const Triangle &other) throw() :
+a(other.a), b(other.b), c(other.c),
+G(other.G),
+n(other.n),
+S(other.S)
+{
+}
+
+void Triangle:: recompute() throw()
+{
+    G = (1.0/3) * (a->r+b->r+c->r);
+    const vtx_t AB(a->r,b->r);
+    const vtx_t AC(a->r,c->r);
+    n = vtx_t::cross_(AB, AC);
+    S = n.norm();
+    n.normalize();
+}
+
+
--- fish/src/fish.hpp
+++ fish/src/fish.hpp
@@ -2,7 +2,69 @@
 #define FISH_INCLUDED 1
 
 #include "yocto/math/dat/b-splines.hpp"
+#include "yocto/math/v3d.hpp"
+#include "yocto/counted-object.hpp"
+#include "yocto/ptr/arc.hpp"
 
+using namespace yocto;
+using namespace math;
+
+typedef CubicApproximation<double,v3d> CubiX;
+typedef CubiX::vtx_t                   vtx_t;
+
+class Point : public counted_object
+{
+public:
+    const size_t i;
+    vtx_t        r;
+    explicit Point() throw();
+    virtual ~Point() throw();
+
+private:
+    static size_t Index;
+};
+
+typedef arc_ptr<Point> pPoint;
+
+
+class Slice : public counted_object
+{
+public:
+    const double   z;
+    const double   perimeter;
+    vector<pPoint> points;
+
+    explicit Slice(double zz,double pr) throw();
+    virtual ~Slice() throw();
+
+private:
+    YOCTO_DISABLE_COPY_AND_ASSIGN(Slice);
+};
+
+typedef arc_ptr<Slice> pSlice;
+
+// Triangle with normal out w.r.t [0 0 0.5]
+class Triangle
+{
+public:
+    pPoint a,b,c;
+    vtx_t  G;
+    vtx_t  n;
+    double S;
+
+    Triangle(const pPoint &A,
+             const pPoint &B,
+             const pPoint &C) throw();
+    
+    Triangle(const Triangle &other) throw();
+    ~Triangle() throw();
+
+    //! after a scaling...
+    void recompute() throw();
+    
+private:
+    YOCTO_DISABLE_ASSIGN(Triangle);
+};
 
 
 #endif
